905f9d41e782b52f2505521933ebec31960d6709
ljeun39 <64400845+ljeun39@users.noreply.github.com>
2021-01-15 07:38:37 +0000

    .create dataType3

---
+++ basic/dataType3.py
@@ -0,0 +1,17 @@
+### 문자열 자료형 ###
+# a = 'Hello world'
+# print(a)
+
+# a = "Don't you know \"Python\"?"
+# print(a)
+
+# 문자열 연산
+a = "Hello"
+b = "World"
+print(a + " " + b)
+
+a = "String"
+print(a * 3)
+
+a = "ABCDEF"
+print(a[2:4])
