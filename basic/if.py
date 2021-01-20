250f12fab6ff32f8c2b24d46dc61f85a15a5ced5
ljeun39 <64400845+ljeun39@users.noreply.github.com>
2021-01-20 12:19:34 +0000

    .210120 if

---
+++ basic/if.py
@@ -0,0 +1,51 @@
+### 조건문
+
+# x = 15
+
+# if x >= 10:
+#   print("x >= 10")
+
+# if x >= 0:
+#   print("x >= 0")
+
+# if x >= 30:
+#   print("x >= 30")
+
+# a = -15
+
+# # 4개의 공백문자 사용하기
+# if a >= 0:
+#     print("a >= 0")
+# elif a >= -10:
+#     print("0 > a >= -10")
+# else:
+#     print("-10 > a")
+
+## 성적 구간에 따른 학점 출력 예제
+# score=85
+
+# if score >= 90:
+#     print("학점 : A")
+# elif score >= 80:
+#     print("학점 : B")
+# elif score >= 70:
+#     print("학점 : C")
+# else:
+#     print("학점 : F")
+
+# if True or False:
+# if True and False:
+#     print("Yes")
+
+# a = 15
+
+# if a <= 20 and a >= 0:
+#     print("Yes")
+
+## pass 키워드
+a = 50
+
+if a >= 30:
+    pass # 나중에 사용하고 싶을 때
+else :
+    print("a < 30")
